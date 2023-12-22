c65930f349eca55be1cdf20a332e41d2132d33d9
mcsuhone <mikko.e.suhonen@gmail.com>
2023-12-22 16:59:26 +0200

    5.18.

--- cypress/e2e/blogilista.cy.js
+++ cypress/e2e/blogilista.cy.js
@@ -1,10 +1,31 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    // cy.request('POST', 'http://localhost:3000/api/testing/reset')
+    cy.request('POST', 'http://localhost:3000/api/testing/reset')
+    cy.request('POST', 'http://localhost:3000/api/users', {
+      username: 'testaaja',
+      name: 'Teppo',
+      password: 'asdasd'
+    })
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.contains('Log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testaaja')
+      cy.get('#password').type('asdasd')
+      cy.get('#login-button').click()
+      cy.contains('testaaja logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testaaja')
+      cy.get('#password').type('123123')
+      cy.get('#login-button').click()
+      cy.contains('wrong username or password')
+    })
   })
 })
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -23,9 +23,9 @@ const LoginForm = ({ handleLogin }) => {
   return (
     <div>
       <form onSubmit={submitLogin}>
-        <div>username <input type='text' onChange={handleUsernameChange} value={username}/></div>
-        <div>password <input type='text' onChange={handlePasswordChange} value={password}/></div>
-        <div><button type='submit'>login</button></div>
+        <div>username <input id='username' type='text' onChange={handleUsernameChange} value={username}/></div>
+        <div>password <input id='password' type='text' onChange={handlePasswordChange} value={password}/></div>
+        <div><button id='login-button' type='submit'>login</button></div>
       </form>
     </div>
   )
